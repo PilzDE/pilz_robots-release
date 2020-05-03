e38b1cefd253a5bbee88c85a9ca5b25053e1a779
Alexander Gutenkunst <alex.gutenkunst+github@gmail.com>
2020-05-03 16:18:12 +0200

    Rebase from 'upstream'

--- prbt_support/src/system_info_exception.cpp
+++ prbt_support/src/system_info_exception.cpp
@@ -15,30 +15,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef MODBUS_MSG_STO_WRAPPER_EXCEPTION_H
-#define MODBUS_MSG_STO_WRAPPER_EXCEPTION_H
+#include <prbt_support/system_info_exception.h>
 
-#include <string>
-#include <stdexcept>
-
-#include <prbt_hardware_support/modbus_msg_wrapper_exception.h>
-
-namespace prbt_hardware_support
+namespace prbt_support
 {
-  /**
-   * @brief Expection thrown upon construction of ModbusMsgStoWrapper of the message
-   * does not contain the required information.
-   *
-   */
-  class ModbusMsgStoWrapperException : public ModbusMsgWrapperException
-  {
-    public:
-      ModbusMsgStoWrapperException( const std::string& what_arg ):
-        ModbusMsgWrapperException(what_arg)
-      {
-      }
-  };
 
+SystemInfoException::SystemInfoException(const std::string &what_arg)
+  : std::runtime_error(what_arg)
+{
 }
 
-#endif // MODBUS_MSG_STO_WRAPPER_EXCEPTION_H
+} // prbt_support
